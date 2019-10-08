753471d5e015da39bb31dfd92788058f980b4f3d
Rory Webber <Rory.Webber@arup.com>
2019-10-08 17:20:10 +0100

    #1 added a couple of classes to make it more OOPy. Everything is messed up rn, but should be working soon

--- Generator.java
+++ Generator.java
@@ -29,13 +29,98 @@ class Generator
         int numRows = (int)Math.round(findNumRows(values.size()-1, 0) + 2);
         int numCols = values.size()+1;
 
-        String[][] table = new String[numRows][numCols];
+        // String[][] table = new String[numRows][numCols];
 
-        addHeaders(table,values);
+        Table table = new Table(numCols, numRows-1);
 
-        fillTable(table, numCols-1, expressionArr, values);
+        String[] headers = findHeaders(values);
 
-        printTable(table, numRows);
+        table.setHeaders(headers);
+
+        table.printTable();
+
+        calcTable(table, expressionArr, values);
+
+        // addHeaders(table,values);
+
+        // fillTable(table, numCols-1, expressionArr, values);
+
+        // printTable(table, numRows);
+    }
+
+    // public static void fillTable(String[][] table, int numCols, String[] expressionArr, ArrayList<String> values) // Calculate the expression and fill the table array
+    // {
+    //     String[] binary = new String[numCols];
+
+    //     Arrays.fill(binary, "0");
+
+    //     for (int r = 1; r < table.length; r++)
+    //     {
+    //         addToRow(table,r,binary);
+
+    //         String result = calcResult(table[r], expressionArr, values);
+
+    //         table[r][numCols] = result;
+
+    //         incBinary(binary);
+    //     }
+    // }
+
+    public static void calcTable(Table table, String[] expressionArr, ArrayList<String> values)
+    {
+        for (int r = 0; r < table.getNumValueRows(); r++)
+        {
+            boolean[] realValues = removeLastIndex(table.getValuesRow(r));
+
+            String result = calcResult(realValues, expressionArr, values);
+        }
+    }
+
+    public static String calcResult(boolean[] row, String[] expressionArr, ArrayList<String> values)
+    {
+        String result = "";
+
+        ValuesData valuesData = new ValuesData(values.size());
+
+        // String[][] valuesData = new String[values.size()][2];
+
+        String[] exp = copyStringArr(expressionArr); // Copying the expression as changing the values in expressionArr here will change it everywhere (yey stack and heap)
+
+        fillValuesData(valuesData, row, values);
+
+        placeValues(exp, valuesData); // Replace the letter values with the check values in this row
+
+        calcExp(exp, 0, exp.length-1);
+
+        result = findResult(exp);
+
+        return result;
+    }
+
+    public static boolean[] removeLastIndex(boolean[] b)
+    {
+        boolean[] res = new boolean[b.length-1];
+
+        for (int i = 0; i < res.length; i++)
+        {
+            res[i] = b[i];
+        }
+
+        return res;
+    }
+
+    public static String[] findHeaders(ArrayList<String> values)
+    {
+        String[] h = new String[values.size()+1];
+
+        for (int i = 0; i < values.size(); i++)
+        {
+            h[i] = values.get(i);
+        }
+
+        h[values.size()] = "Result";
+
+        return h;
     }
 
     public static String[] splitExpression(String expression)
@@ -88,42 +173,26 @@ class Generator
         return num;
     }
 
-    public static void fillTable(String[][] table, int numCols, String[] expressionArr, ArrayList<String> values) // Calculate the expression and fill the table array
-    {
-        String[] binary = new String[numCols];
+    
 
-        Arrays.fill(binary, "0");
+    // public static String calcResult(String[] row, String[] expressionArr, ArrayList<String> values)
+    // {
+    //     String result = "";
 
-        for (int r = 1; r < table.length; r++)
-        {
-            addToRow(table,r,binary);
+    //     String[][] valuesData = new String[values.size()][2];
 
-            String result = calcResult(table[r], expressionArr, values);
+    //     String[] exp = copyStringArr(expressionArr); // Copying the expression as changing the values in expressionArr here will change it everywhere (yey stack and heap)
 
-            table[r][numCols] = result;
+    //     fillValuesData(valuesData, row, values);
 
-            incBinary(binary);
-        }
-    }
+    //     placeValues(exp, valuesData); // Replace the letter values with the check values in this row
 
-    public static String calcResult(String[] row, String[] expressionArr, ArrayList<String> values)
-    {
-        String result = "";
+    //     calcExp(exp, 0, exp.length-1);
 
-        String[][] valuesData = new String[values.size()][2];
+    //     result = findResult(exp);
 
-        String[] exp = copyStringArr(expressionArr); // Copying the expression as changing the values in expressionArr here will change it everywhere (yey stack and heap)
-
-        fillValuesData(valuesData, row, values);
-
-        placeValues(exp, valuesData); // Replace the letter values with the check values in this row
-
-        calcExp(exp, 0, exp.length-1);
-
-        result = findResult(exp);
-
-        return result;
-    }
+    //     return result;
+    // }
 
     public static String findResult(String[] exp)
     {
@@ -219,7 +288,7 @@ class Generator
         return -1;
     }
 
-    public static void placeValues(String[] exp, String[][] valuesData)
+    public static void placeValues(String[] exp, ValuesData valuesData)
     {
         for (int i = 0; i < exp.length; i++)
         {
@@ -227,11 +296,11 @@ class Generator
         }
     }
 
-    public static String getCheckValue(String[][] valuesData, String value)
+    public static String getCheckValue(ValuesData valuesData, String value)
     {
-        for (int i = 0; i < valuesData.length; i++)
+        for (int i = 0; i < valuesData.size(); i++)
         {
-            if (valuesData[i][0].equals(value)) return valuesData[i][1];
+            if (valuesData.getValue(i).equals(value)) return boolToString(valuesData.getData(i));
         }
 
         return "null";
@@ -249,12 +318,14 @@ class Generator
         return newArr;
     }
 
-    public static void fillValuesData(String[][] valuesData, String[] row, ArrayList<String> values)
+    public static void fillValuesData(ValuesData valuesData, boolean[] row, ArrayList<String> values)
     {
         for (int v = 0; v < values.size(); v++)
         {
-            valuesData[v][0] = values.get(v);
-            valuesData[v][1] = row[v];
+            valuesData.setValue(v, values.get(v));
+            valuesData.setData(v, row[v]);
+            // valuesData[v][0] = values.get(v);
+            // valuesData[v][1] = row[v];
         }
     }
 
@@ -309,15 +380,15 @@ class Generator
         }
     }
 
-    public static void addHeaders(String[][] table, ArrayList<String> values)
-    {
-        for (int i = 0; i < values.size(); i++)
-        {
-            table[0][i] = values.get(i);
-        }
+    // public static void addHeaders(String[][] table, ArrayList<String> values)
+    // {
+    //     for (int i = 0; i < values.size(); i++)
+    //     {
+    //         table[0][i] = values.get(i);
+    //     }
 
-        table[0][values.size()] = "Result";
-    }
+    //     table[0][values.size()] = "Result";
+    // }
 
     public static double findNumRows(int size, double total) // Finds the number of columns required for the logic table. Will calculate by finding max binary number possible with binary number length "size" + 1 (+1 is done outside of function)
     {
@@ -419,5 +490,149 @@ class Generator
             if (c.equals(arr[i])) return true;
         }
         return false;
+    }
+}
+
+class ValuesData
+{
+    String[] values;
+    boolean[] data;
+
+    public ValuesData(int size)
+    {
+        values = new String[size];
+        data = new boolean[size];
+    }
+
+    public String getValue(int i)
+    {
+        return values[i];
+    }
+
+    public boolean getData(int i)
+    {
+        return data[i];
+    }
+
+    public int size()
+    {
+        return values.length;
+    }
+
+    public void setValue(int i, String val)
+    {
+        values[i] = val;
+    }
+
+    public void setData(int i, boolean d)
+    {
+        data[i] = d;
+    }
+}
+
+class Table
+{
+    String[] headers;
+    boolean[][] values;
+
+    int numValueRows;
+
+    public Table(int numCols, int numRows)
+    {
+        headers = new String[numCols];
+        values = new boolean[numRows][numCols];
+
+        numValueRows = numRows;
+
+        fillValues(values);
+    }
+
+    public int getNumValueRows()
+    {
+        return numValueRows;
+    }
+
+    public boolean[] getValuesRow(int i)
+    {
+        return values[i];
+    }
+
+    public void setHeaders(String[] h)
+    {
+        headers = h;
+    }
+
+    public void printTable()
+    {
+        String row = "| ";
+
+        for (int i = 0; i < headers.length; i++)
+        {
+            row += headers[i] + " | ";
+        }
+
+        System.out.println(row);
+
+        for (int r = 0; r < values.length; r++)
+        {
+            row = "| ";
+
+            for (int c = 0; c < values[1].length; c++)
+            {
+                row += boolToString(values[r][c]) + " | ";
+            }
+
+            System.out.println(row);
+        }
+    }
+
+    public static void fillValues(boolean[][] values)
+    {
+        boolean[] binary = new boolean[values[1].length-1];
+
+        Arrays.fill(binary, false);
+
+        for (int r = 0; r < values.length; r++)
+        {
+            for (int c = 0; c < binary.length; c++)
+            {
+                values[r][c] = binary[c];
+            }
+
+            incBinary(binary);
+        }
+    }
+
+    public static void incBinary(boolean[] binary) // Increment a binary number by 1
+    {
+        int len = binary.length;
+
+        if (!binary[len-1])
+        {
+            binary[len-1] = true;
+
+            return;
+        }
+
+        binary[len-1] = false;
+
+        for (int i = len-2; i >= 0; i--)
+        {
+            if (!binary[i])
+            {
+                binary[i] = true;
+
+                return;
+            }
+            else binary[i] = false;
+        }
+
+        return;
+    }
+
+    public static String boolToString(boolean b)
+    {
+        if (b) return "1";
+        else return "0";
     }
 }
